698f1b480d643efb0e4eca554413ae24271a5639
Antoine <Antoine@Antoine-Laptop>
2014-11-12 00:00:59 +0100

    -> intègre squelette de la classe Artist

--- Seadili/0_model/Artist.php
+++ Seadili/0_model/Artist.php
@@ -1,1 +1,27 @@
 <?php
+
+include_once 'DataBase.php';
+
+class Artist {
+
+    // Attributs
+
+    public function __construct() {}
+
+    // Getter magique
+    public function __get($attr_name) {}
+
+    // Setter magique
+    public function __set($attr_name, $attr_val) {}
+
+    public function update() {}
+
+    public function delete() {}
+
+    public function insert() {}
+
+    public static function findById($id) {}
+
+    public static function findAll() {}
+
+}
