8a4adc6dec6684f4c3b25d7eed787eaba966a30e
Antoine <Antoine@Antoine-Laptop>
2014-11-13 22:41:27 +0100

    -> finalise la classe Artist

--- Seadili/0_model/Artist.php
+++ Seadili/0_model/Artist.php
@@ -53,7 +53,7 @@ class Artist {
 
             // Récupération d'une connexion à la base
             $db = DataBase::getConnection();
-            
+
             // Création de la requête préparée
             $query = "UPDATE Artist SET name = :name , image_url = :imageurl, info = :info WHERE artist_id = :id";
             $statement = $db->prepare($query);
@@ -61,7 +61,7 @@ class Artist {
             $statement->bindParam(':imageurl', $this->image_url);
             $statement->bindParam(':info', $this->info);
             $statement->bindParam(':id', $this->artist_id);
-            
+
             // Exécution de la requête préparée
             $res = $statement->execute();
             return $res;
@@ -71,20 +71,141 @@ class Artist {
         }
     }
 
+    /**
+     * supprime le tuple de la table 'Artist'
+     * qui correspond à l'objet courant
+     * (même artist_id)
+     * @return Booléen à TRUE en cas de succés, FALSE en cas d'échec
+     * @throws Exception si problème pendant exécution de la méthode
+     */
     public function delete() {
-        
+        try {
+            if (!isset($this->artist_id)) {
+                throw new Exception(__CLASS__ . " : Clé primaire non définie : delete impossible");
+            }
+
+            // Récupération d'une connexion à la base
+            $db = DataBase::getConnection();
+
+            // Création de la requête préparée
+            $query = "DELETE Artist WHERE artist_id = :id";
+            $statement = $db->prepare($query);
+            $statement->bindParam(':id', $this->artist_id);
+
+            // Exécution de la requête préparée
+            $res = $statement->execute();
+            return $res;
+        } catch (Exception $e) {
+            $trace = $e->getTrace();
+            echo "Erreur pendant delete: $trace";
+        }
     }
 
+    /**
+     * insère le tuple de la table 'Artist'
+     * qui correspond à l'objet courant
+     * (même artist_id)
+     * @return Booléen à TRUE en cas de succés, FALSE en cas d'échec
+     * @throws Exception si problème pendant exécution de la méthode
+     */
     public function insert() {
-        
+        try {
+            // Récupération d'une connexion à la base
+            $db = DataBase::getConnection();
+
+            // Création de la requête préparée
+            $query = "INSERT INTO Artist (name,image_url,info) VALUES(:name,:imageurl,:info)";
+            $statement = $db->prepare($query);
+            $statement->bindParam(':name', $this->name);
+            $statement->bindParam(':imageurl', $this->image_url);
+            $statement->bindParam(':info', $this->info);
+
+            // Exécution de la requête préparée
+            $res = $statement->execute();
+            return $res;
+        } catch (Exception $e) {
+            $trace = $e->getTrace();
+            echo "Erreur pendant insert: $trace";
+        }
     }
 
+    /**
+     * retourne dans un objet Artist
+     * les informations de la base
+     * relative à l'artiste dont l'id
+     * est celui donné en paramètre
+     * @param $id identifiant de l'ariste dans la base
+     * @return un objet Artist rempli avec les informations contenues dans la base
+     */
     public static function findById($id) {
-        
+        try {
+            // Récupération d'une connexion à la base
+            $db = DataBase::getConnection();
+
+            // Création de la requête préparée
+            $query = "SELECT * FROM Artist WHERE artist_id = :id";
+            $statement = $db->prepare($query);
+            $statement->bindParam(':id', $this->artist_id);
+
+            // Exécution de la requête préparée
+            $statement->execute();
+
+            // Récupération du tuple correspondant à l'id en paramètre
+            $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+            // Remplissage d'un objet Artist avec les informations contenues dans le tuple
+            $artist = new Artist();
+            $artist->artist_id = $row['artist_id'];
+            $artist->name = $row['name'];
+            $artist->image_url = $row['image_url'];
+            $artist->info = $row['info'];
+
+            // Retour de l'artiste
+            return $artist;
+        } catch (Exception $e) {
+            $trace = $e->getTrace();
+            echo "Erreur pendant findById: $trace";
+        }
     }
 
+    /**
+     * retourne dans un tableau d'objets Artist
+     * tous les artistes contenus dans la base
+     * @return un tableau d'objets Artist rempli avec les artistes contenues dans la base
+     */
     public static function findAll() {
-        
+        try {
+            // Récupération d'une connexion à la base
+            $db = DataBase::getConnection();
+
+            // Création de la requête préparée
+            $query = "SELECT * FROM Artist";
+            $statement = $db->prepare($query);
+
+            // Exécution de la requête préparée
+            $statement->execute();
+
+            // Récupération de tous les tuples de la table Artist
+            $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+            $tab = Array();
+            // Tant que des lignes sont retournées
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                // Remplissage d'un objet Artist avec les informations contenues dans le tuple courant
+                $artist = new Artist();
+                $artist->artist_id = $row['artist_id'];
+                $artist->name = $row['name'];
+                $artist->image_url = $row['image_url'];
+                $artist->info = $row['info'];
+                $tab[] = $artist;
+            }
+
+            // Retour du tableau d'artiste
+            return $tab;
+        } catch (Exception $e) {
+            $trace = $e->getTrace();
+            echo "Erreur pendant findAll: $trace";
+        }
     }
 
 }
