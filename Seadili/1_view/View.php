4fcce8fbf5f9190d74a6a83a63e66719963ac2e0
antoine <antoine@Antoine-Laptop>
2014-12-14 22:53:04 +0100

    -> améliore le travail sur le nouveau style

--- Seadili/1_view/View.php
+++ Seadili/1_view/View.php
@@ -18,9 +18,9 @@ class View {
 
     public function defaultView() {
         include '3_content/header.php';
-        
+
         include '3_content/home.html';
-        
+
         echo "<div style=\"display:inline-block; float:left; width:58%; \">";
         echo "<h2>Titres</h2><hr><br><div id=\"resultatsTitres\">";
         foreach (Track::findRandom(3) as $track) {
@@ -39,17 +39,17 @@ class View {
           echo "</div>"; */
         echo "</div>";
 
-        echo "<div style=\"display:inline-block; float:right; width:39%; margin:auto;\">";
-        echo "<h2>Votre écoute</h2><hr><br>";
-        include '3_content/player.html';
-        echo "\n";
-        echo "</div>";
+        /* echo "<div style=\"display:inline-block; float:right; width:39%; margin:auto;\">";
+          echo "<h2>Votre écoute</h2><hr><br>";
+          include '3_content/player.html';
+          echo "\n";
+          echo "</div>"; */
 
         echo "<div style=\"display:inline-block; float:right; width:39%; margin:auto;\">";
         echo "<h2>Votre nouvelle playlist</h2><hr><br>";
         include '3_content/newplaylist.html';
         echo "</div>";
-        
+
         echo "<div style=\"display:inline; float:right; width:100%;\">";
         include '3_content/footer.html';
         echo "</div>";
@@ -139,10 +139,18 @@ class View {
      */
     public function artistView($artist) {
         echo"<div class=\"artiste\">" . $artist->name . "
-        <br><img src=\"" . $artist->image_url . "\" height=\"100px\"/>
-        <!--<br>Info : " . $artist->info . "-->
-        <br><input type=\"image\" height=\"20px\" src=\"http://localhost/Seadili/Seadili/5_images/info.png\" value=\"Info\">
-        </div>";
+        <br><img src=\"" . $artist->image_url . "\" height=\"100px\"/>";
+        echo "<br><button style=\"background-color: white; border: 0px;\" class=\"easy-modal-open\" href=\"#artistinfo" . $artist->artist_id . "\"><img height=\"20px\" src=\"http://localhost/Seadili/Seadili/5_images/info.png\" height=\"100px\"/></button>\n";
+        /* echo "<!--<br>Info : " . $artist->info . "-->
+          <br><input type=\"image\" height=\"20px\" src=\"http://localhost/Seadili/Seadili/5_images/info.png\" value=\"Info\">"; */
+        echo "</div>";
+        
+        echo "<div class=\"easy-modal\" id=\"artistinfo" . $artist->artist_id . "\">\n";
+        echo "<br><div class=\"infoartist\">";
+        echo "<b>" . $artist->name . "</b>";
+        echo "<br><br><img src=\"" . $artist->image_url . "\" height=\"100px\"/>";
+        echo "<br><br><i>" . $artist->info . "</i></div>";
+        echo "</div>\n";
     }
 
     /**
@@ -169,7 +177,7 @@ class View {
      * Affiche un titre
      * @param $track Titre à afficher
      */
-    public function trackView($track) {
+    public function trackViewGOOD($track) {
         echo"<div class=\"morceau\">" . $track->title;
         echo
         "<form name=\"addtrack\" method=\"POST\">
@@ -180,6 +188,20 @@ class View {
         <!--<br><audio controls=\"controls\"><source src=" . $track->mp3_url . "/></audio>-->
         <br><input type=\"button\" value=\"Lire\" onclick=\"playTrack('" . $track->mp3_url . "')\">
         </div>";
+    }
+
+    /**
+     * Affiche un titre
+     * @param $track Titre à afficher
+     */
+    public function trackView($track) {
+        $artist = Artist::findById($track->artist_id);
+        echo"<div class=\"morceau\">";
+        echo "<input type=\"image\" height=\"25px\" src=\"http://localhost/Seadili/Seadili/5_images/play.png\" value=\"Lire\" onclick=\"playTrack('" . $track->mp3_url . "')\"><br>";
+        echo $track->title;
+        echo "<br><i>" . $artist->name . "</i>";
+        echo "<br><input type=\"image\" height=\"27px\" src=\"http://localhost/Seadili/Seadili/5_images/add.png\" value=\"Ajouter\" onclick=\"addtrackplaylist(" . $track->track_id . ",'" . $track->title . "')\">";
+        echo "</div>";
     }
 
     /**
--- Seadili/3_content/header.php
+++ Seadili/3_content/header.php
@@ -12,10 +12,38 @@
         <link href='http://fonts.googleapis.com/css?family=Advent+Pro' rel='stylesheet' type='text/css'>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script src="js/audioplayer.js"></script>
+        <script type="text/javascript" src="js/jquery.easyModal.js"></script>
         <script>$(function () {
                 $('audio').audioPlayer();
             });</script>
 
+	<script type="text/javascript">
+	$(function() {
+		$('.easy-modal').easyModal({
+			top: 200,
+			overlay: 0.2
+		});
+
+		$('.easy-modal-open').click(function(e) {
+			var target = $(this).attr('href');
+			$(target).trigger('openModal');
+			e.preventDefault();
+		});
+
+		$('.easy-modal-close').click(function(e) {
+			$('.easy-modal').trigger('closeModal');
+		});
+
+		$('.easy-modal-animated').easyModal({
+			top: 200,
+			overlay: 0.2,
+			transitionIn: 'animated bounceInLeft',
+			transitionOut: 'animated bounceOutRight',
+			closeButtonClass: '.animated-close'
+		});
+	});
+	</script>   
+        
         <script>
             function playTrack(mp3url) {
                 console.log("toto");
@@ -98,12 +126,9 @@
     </head>
 
     <body>
-
-        <?php include '3_content/connecting_bar.php'; ?>
-
-        <title>
-            SeaDiLi 
-        </title>
+ 
+        
+        <!-- <?php include '3_content/connecting_bar.php'; ?> -->
 
         <!--<div class="player">
             <audio preload="auto" src="http://freedownloads.last.fm/download/86630288/Omnipresent.mp3" controls>
@@ -111,9 +136,16 @@
         </div>-->
 
         <div class="titre"> 
-            <h1>SeaDiLi</h1>
-            <h4>Search.Discover.Listen</h4>
+            <img src="http://localhost/Seadili/Seadili/5_images/logo/tmp2.jpg"/>
         </div>
+
+        <?php
+        echo "<div style=\"display:inline-block; float:right; width:60%; margin:auto;\">";
+        echo "<h2>Votre écoute</h2><hr><br>";
+        include '3_content/player.html';
+        echo "\n";
+        echo "</div>";
+        ?>
 
         <div class="menu">
             <a href="index.php?a=home" ><div id ="h" class="lienhome"><img onclick="menu('home')" height="35px" src="http://localhost/Seadili/Seadili/5_images/home.png"/></div></a>
