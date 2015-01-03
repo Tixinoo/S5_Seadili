d9094c465808d3ee1058aea7948da3f6918eda13
antoine <antoine@Antoine-Laptop>
2015-01-03 23:29:41 +0100

    -> corrige des commentaires du contenu

--- Seadili/3_content/connecting_bar.php
+++ Seadili/3_content/connecting_bar.php
@@ -1,25 +1,30 @@
 <?php session_start();
 
+// Barre de connexion
+
+// Si l'utilisateur est connecté
 if (isset($_SESSION['username'])) {
 
-    echo "        <div class=\"connexion\">\n";
+    // On affiche un message, son nom d'utilisateur et un bouton pour se déconnecter
+    echo "<div class=\"connexion\">\n";
     echo "Vous êtes connectés en tant que " . $_SESSION['username'];
     echo "<form action=\"3_content/disconnection.php\" method=\"GET\">\n";
-    echo "  <input class=\"bouton\" type=\"submit\" value=\"Se déconnecter\"/>\n";
+    echo "<input class=\"bouton\" type=\"submit\" value=\"Se déconnecter\"/>\n";
     echo "</form>\n";
-    echo "        </div>\n";
+    echo "</div>\n";
     
 } else {
 
-    echo "        <div class=\"connexion\">\n";
-    echo "            <div class=\"input\">\n";
-    echo "                <form action=\"3_content/connection.php\" method=\"post\">\n";
-    echo "                    <input class=\"champ\" type=\"text\" name=\"username\" value=\"bateau\" size=\"20\"/>\n";
-    echo "                    <input class=\"champ\" type=\"password\" name=\"password\" value=\"mdp\"/>\n";
-    echo "                    <input class=\"bouton\" type=\"submit\" value=\"Se connecter\"/>\n";
-    echo "                </form>\n";
-    echo "            </div>\n";
-    echo "        </div>\n";
+    // Sinon, on affiche des champs et un bouton pour qu'il puisse se connecter
+    echo "<div class=\"connexion\">\n";
+    echo "<div class=\"input\">\n";
+    echo "<form action=\"3_content/connection.php\" method=\"post\">\n";
+    echo "<input class=\"champ\" type=\"text\" name=\"username\" value=\"bateau\" size=\"20\"/>\n";
+    echo "<input class=\"champ\" type=\"password\" name=\"password\" value=\"mdp\"/>\n";
+    echo "<input class=\"bouton\" type=\"submit\" value=\"Se connecter\"/>\n";
+    echo "</form>\n";
+    echo "</div>\n";
+    echo "</div>\n";
     
 }
 
